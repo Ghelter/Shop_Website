c5866b22b3fe7163e4d80a745c4435804adfb037
Andres Siri <44003042+Leeth44@users.noreply.github.com>
2020-04-20 21:05:39 -0400

    changes to css/stylesheets - website color
    thankyou.html added into newsletter.php + button back to HomePage

--- newsletter.php
+++ newsletter.php
@@ -4,38 +4,46 @@ $username = "id13191005_storageemail";
 $password = "7XRK11ISWkSRG4^=";
 $database = "id13191005_emaildb";
 
-$fname = stripslasher($_POST['fname']);
-$lname = stripslasher($_POST['lname']);
-$displayContents = $_REQUEST[""];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
 $useremail = $_POST['email'];
-$TableName = "subscribers";
-
-/*
-if (preg_match("~([a-zA-Z0-9!#$%&'*+-/=?^_`{|}~])@([a-zA-Z0-9-]).([a-zA-Z0-9]{2,4})~", $useremail)) {
-    $useremail = $_POST['email'];
-} else {
-    echo "the email $useremail is invalid";
-}
-*/
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
-
+/*
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
+*/
+//$conn = mysqli_connect($servername, $username, $password, $database);
 
-$SQLs = "INSERT INTO subscribers (fname, lname, Semail) VALUES ($fname, $lname, $useremail)";
-
-
+$SQLs = "INSERT INTO subscribers (fname, lname, Semail) VALUES ('$fname', '$lname', '$useremail');";
+/*
 if (mysqli_query($conn, $SQLs)) {
 echo "<p>Successfully added the record.</p>";
 }
 else {     
 echo "<p>Unable to execute the query.</p>";
 }
-
-
+*/
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css2?family=Bangers&family=Open+Sans&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="thankyou.css">
+<title>Thank you!</title>
+</head>
+<body>
+
+<p>Thank you for subscribing to our Newsletters!</p>
+
+<a href="index.html">Back To HomePage</a>
+
+</body>
+</html>
